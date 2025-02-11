f1b5b6b8d63be7d6316f35082cc264929929d08e
BJD23 <124942957+BJD23@users.noreply.github.com>
2025-02-11 09:36:44 -0600

    Reestructuración de carpetas 2

---
+++ principios_de_construccion/Actividad 1/Main.java
@@ -0,0 +1,10 @@
+//Actividad 1. Convenciones y código limpio (clean code)
+//Descripción:
+//Elabora programa en Java para llevar el control del personal de la selección Mexicana de Futbol. Considera los siguientes diagramas e implementa la herencia para disminuir 
+//la duplicidad de código y aplica los principios del código limpio. Utiliza las convenciones de código "Google Java Style Guide" y describe dentro del código (con comentarios) 
+//la regla aplicada con base en la guía; mínimo 5 reglas. https://eminus.uv.mx/eminusapi/froala/cur_60174/Actividades/act_400103/froala/23eac07b-f27b-4a6d-addb-d5425c672c22.jpg
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
